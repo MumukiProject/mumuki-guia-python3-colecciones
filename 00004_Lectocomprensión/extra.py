11304c2d5284b3b41d10698441f434bbf3efeb19
mayra@mumuki.org <mayra@mumuki.org>
2022-01-11 19:26:33 +0000

    Mumuki Export on 2022-01-11 19:26:33 +0000

---
+++ 00004_Lectocomprensión/extra.py
@@ -0,0 +1,7 @@
+un_libro_corto = Libro("Socorro", 299, "Terror")
+segundo_libro_corto = Libro("Fundación", 300, "Ciencia ficción")
+tercer_libro_corto = Libro("Historias de cronopios y de famas", 153, "Cuento y novela")
+un_libro_largo = Libro("Curtis Biología", 1164, "Ciencia")
+segundo_libro_largo = Libro("El nudo de la conciencia", 464, "Divulgación")
+tercer_libro_largo = Libro("La insoportable levedad del ser", 336, "Novela")
+cuarto_libro_largo = Libro("El hobbit", 310, "Novela")
--- 00004_Lectocomprensión/test.py
+++ 00004_Lectocomprensión/test.py
@@ -1,6 +1,16 @@
   
   def test_Si_a_una_biblioteca_con_tres_libros_largos_le_enviamos_el_mensaje_libros_largos_nos_retorna_esos_tres_libros(self):
     biblioteca = Biblioteca()
-    biblioteca.agregar_libro("Socorro")
-    biblioteca.agregar_libro("El lobo estepario")
     self.assertEqual(biblioteca.libros, ["Socorro", "El lobo estepario"])
+    
+    def test_un_libro_con_menos_de_300_paginas_no_es_largo(self):
+    un_libro_corto = Libro("Socorro", 299, "Terror")
+    self.assertFalse(un_libro_corto.es_largo())
+
+  def test_un_libro_con_300_paginas_no_es_largo(self):
+    otro_libro_corto = Libro("Fundación", 300, "Ciencia ficción")
+    self.assertFalse(otro_libro_corto.es_largo())
+
+  def test_un_libro_con_mas_de_300_paginas_es_largo(self):
+    otro_libro_corto = Libro("Fundación e imperio", 301, "Ciencia ficción")
+    self.assertTrue(otro_libro_corto.es_largo())
