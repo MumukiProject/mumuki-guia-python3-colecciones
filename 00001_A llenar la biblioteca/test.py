a11052a782dd7101f6992e0772626ef4e7cb0d9b
gustavo@mumuki.org <gustavo@mumuki.org>
2022-01-11 14:54:27 +0000

    Mumuki Export on 2022-01-11 14:54:27 +0000

--- 00001_A llenar la biblioteca/test.py
+++ 00001_A llenar la biblioteca/test.py
@@ -1,4 +1,17 @@
-class Test(unittest.TestCase):
 
-  def test_description_example(self):
-    self.assertTrue(True)
+  def test_una_biblioteca_arranca_sin_libros(self):
+    biblioteca = Biblioteca()
+    self.assertEqual(biblioteca.libros, [])
+
+  def test_si_una_biblioteca_recibe_el_mensaje_agregar_libro_se_agrega_un_libro_a_sus_libros(self):
+    biblioteca = Biblioteca()
+    biblioteca.agregar_libro("Fundación")
+    self.assertEqual(biblioteca.libros, ["Fundación"])
+
+  def test_si_una_biblioteca_recibe_el_mensaje_agregar_libro_dos_veces_se_agregan_dos_libros_a_sus_libros(self):
+    biblioteca = Biblioteca()
+    biblioteca.agregar_libro("Socorro")
+    biblioteca.agregar_libro("El lobo estepario")
+    self.assertEqual(biblioteca.libros, ["Socorro", "El lobo estepario"])
+    
+  
