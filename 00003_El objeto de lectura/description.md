e52873cf53518436a99bae008f11baae4655cc0f
gustavo@mumuki.org <gustavo@mumuki.org>
2022-01-11 15:05:03 +0000

    Mumuki Export on 2022-01-11 15:05:03 +0000

--- 00003_El objeto de lectura/description.md
+++ 00003_El objeto de lectura/description.md
@@ -1,6 +1,6 @@
 Por lo que vimos en el ejercicio anterior, los libros son strings. Lamentablemente, esto limita bastante nuestro modelado y las cosas que podemos hacer con ellos. :cry:
 
-Por este motivo queremos crear la clase `Libro` :books:, para poder saber no sólo su título sino también la cantidad de páginas y el género. En ese caso, el ejemplo del ejercicio anterior quedaría así:
+Por este motivo queremos crear la clase `Libro` :books:, para poder saber no sólo su `titulo` sino también su cantidad de `paginas` y el `genero`. En ese caso, el ejemplo del ejercicio anterior quedaría así:
 
 ```python
 ム libro_1 = Libro("La insoportable levedad del ser", 330, "Drama")
@@ -14,4 +14,4 @@ Por este motivo queremos crear la clase `Libro` :books:, para poder saber no só
 ム biblioteca_de_mar.agregar_libro(libro_3)
 ```
 
-> Definí la clase `Libro` con su respectivo constructor. Los libros tienen que entender el mensaje `es_largo` que nos dice si tienen más de 300 páginas. ¿Y qué pasa con la clase `Biblioteca`?¿hay que cambiar algo? :eyes:
+> Definí la clase `Libro` con su respectivo constructor. Los libros tienen que entender el mensaje `es_largo` que nos dice si tienen más de 300 `paginas`. ¿Y qué pasa con la clase `Biblioteca`?¿hay que cambiar algo? :eyes:
--- 00003_El objeto de lectura/test.py
+++ 00003_El objeto de lectura/test.py
@@ -1,4 +1,28 @@
-class Test(unittest.TestCase):
 
-  def test_description_example(self):
-    self.assertTrue(True)
+  def test_si_creo_un_nuevo_Libro_le_puedo_especificar_su_titulo_cantidad_de_páginas_y_género(self):
+    un_libro = Libro("Socorro", 250, "Terror")
+    self.assertEqual(un_libro.titulo, "Socorro")
+    self.assertEqual(un_libro.paginas, 250)
+    self.assertEqual(un_libro.genero, "Terror")
+
+  def test_un_libro_con_menos_de_300_paginas_no_es_largo(self):
+    un_libro_corto = Libro("Socorro", 299, "Terror")
+    self.assertFalse(un_libro_corto.es_largo())
+
+  def test_un_libro_con_300_paginas_no_es_largo(self):
+    otro_libro_corto = Libro("Fundación", 300, "Ciencia ficción")
+    self.assertFalse(otro_libro_corto.es_largo())
+
+  def test_un_libro_con_mas_de_300_paginas_es_largo(self):
+    otro_libro_corto = Libro("Fundación e imperio", 301, "Ciencia ficción")
+    self.assertTrue(otro_libro_corto.es_largo())
+
+  def test_una_biblioteca_arranca_sin_libros(self):
+    biblioteca = Biblioteca()
+    self.assertEqual(biblioteca.libros, [])
+
+  def test_si_una_biblioteca_recibe_el_mensaje_agregar_libro_se_agrega_un_libro_a_sus_libros(self):
+    biblioteca = Biblioteca()
+    un_libro = Libro("Socorro", 250, "Terror")
+    biblioteca.agregar_libro(un_libro)
+    self.assertEqual(biblioteca.libros, [un_libro])
