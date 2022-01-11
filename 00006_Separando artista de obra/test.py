6f283c8f24cb6b5ce9acba68c818b8f9b92d78c9
gustavo@mumuki.org <gustavo@mumuki.org>
2022-01-11 20:25:45 +0000

    Mumuki Export on 2022-01-11 20:25:45 +0000

--- 00006_Separando artista de obra/test.py
+++ 00006_Separando artista de obra/test.py
@@ -1,4 +1,12 @@
-class Test(unittest.TestCase):
 
-  def test_description_example(self):
-    self.assertTrue(True)
+  def test_si_creo_un_nuevo_Libro_le_puedo_especificar_su_artista(self):
+    un_libro = Libro("Socorro", 250, "Terror", {"nombre": "Elsa", "apellido": "Bornemann", "nacionalidad": "Argentina" })
+    self.assertEqual(un_libro.artista, {"nombre": "Elsa", "apellido": "Bornemann", "nacionalidad": "Argentina" })
+   
+  def test_un_libro_con_una_artista_de_argentina_tiene_nacionalidad_argentina(self):
+    un_libro_corto = Libro("Socorro", 299, "Terror", {"nombre": "Elsa", "apellido": "Bornemann", "nacionalidad": "Argentina" })
+    self.assertEqual(un_libro.nacionalidad, "Argentina")
+
+  def test_un_libro_con_una_artista_de_mexico_tiene_nacionalidad_mexico(self):
+    un_libro_corto = Libro("Como agua para chocolate", 216, "Realismo mágico", {"nombre": "Laura", "apellido": "Esquivel", "nacionalidad": "México" })
+    self.assertEqual(un_libro.nacionalidad, "México")
