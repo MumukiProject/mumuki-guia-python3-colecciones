bd3f09b1566cee94f8cfe7822f00208401bd86b1
mayra@mumuki.org <mayra@mumuki.org>
2022-01-11 19:28:46 +0000

    Mumuki Export on 2022-01-11 19:28:45 +0000

--- 00004_Lectocomprensión/test.py
+++ 00004_Lectocomprensión/test.py
@@ -1,16 +1,5 @@
   
   def test_Si_a_una_biblioteca_con_tres_libros_largos_le_enviamos_el_mensaje_libros_largos_nos_retorna_esos_tres_libros(self):
     biblioteca = Biblioteca()
-    self.assertEqual(biblioteca.libros, ["Socorro", "El lobo estepario"])
-    
-    def test_un_libro_con_menos_de_300_paginas_no_es_largo(self):
-    un_libro_corto = Libro("Socorro", 299, "Terror")
-    self.assertFalse(un_libro_corto.es_largo())
-
-  def test_un_libro_con_300_paginas_no_es_largo(self):
-    otro_libro_corto = Libro("Fundación", 300, "Ciencia ficción")
-    self.assertFalse(otro_libro_corto.es_largo())
-
-  def test_un_libro_con_mas_de_300_paginas_es_largo(self):
-    otro_libro_corto = Libro("Fundación e imperio", 301, "Ciencia ficción")
-    self.assertTrue(otro_libro_corto.es_largo())
+    biblioteca.libros = [un_libro_largo, segundo_libro_largo, tercer_libro_largo, un_libro_corto]
+    self.assertEqual(biblioteca.libros_largos(), [un_libro_largo, segundo_libro_largo, tercer_libro_largo])
