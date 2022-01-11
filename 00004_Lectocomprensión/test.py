2bd378591ac76fb114c28dd60cc20fa0df00bd06
gustavo@mumuki.org <gustavo@mumuki.org>
2022-01-11 20:13:36 +0000

    Mumuki Export on 2022-01-11 20:13:36 +0000

--- 00004_Lectocomprensión/test.py
+++ 00004_Lectocomprensión/test.py
@@ -1,4 +1,4 @@
-class Test(unittest.TestCase):
+
   def setUp(self):
     un_libro_corto = Libro("Socorro", 299, "Terror")
     segundo_libro_corto = Libro("Fundación", 300, "Ciencia ficción")
@@ -9,6 +9,7 @@ class Test(unittest.TestCase):
     cuarto_libro_largo = Libro("El hobbit", 310, "Novela")
     
   def test_Si_a_una_biblioteca_con_tres_libros_largos_le_enviamos_el_mensaje_libros_largos_nos_retorna_esos_tres_libros(self):
+    self.setUp():
     biblioteca = Biblioteca()
     biblioteca.libros = [un_libro_largo, segundo_libro_largo, tercer_libro_largo, un_libro_corto]
     self.assertEqual(biblioteca.libros_largos(), [un_libro_largo, segundo_libro_largo, tercer_libro_largo])
