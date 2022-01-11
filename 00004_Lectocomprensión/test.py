07f88b0fa53710eb7847ff83983bdfc142f11157
gustavo@mumuki.org <gustavo@mumuki.org>
2022-01-11 20:13:49 +0000

    Mumuki Export on 2022-01-11 20:13:49 +0000

--- 00004_Lectocomprensión/test.py
+++ 00004_Lectocomprensión/test.py
@@ -9,7 +9,7 @@
     cuarto_libro_largo = Libro("El hobbit", 310, "Novela")
     
   def test_Si_a_una_biblioteca_con_tres_libros_largos_le_enviamos_el_mensaje_libros_largos_nos_retorna_esos_tres_libros(self):
-    self.setUp():
+    self.setUp()
     biblioteca = Biblioteca()
     biblioteca.libros = [un_libro_largo, segundo_libro_largo, tercer_libro_largo, un_libro_corto]
     self.assertEqual(biblioteca.libros_largos(), [un_libro_largo, segundo_libro_largo, tercer_libro_largo])
