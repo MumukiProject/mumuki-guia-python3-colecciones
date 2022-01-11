3ef273f1babcb04db2f8ef69a3bfa01135104031
gustavo@mumuki.org <gustavo@mumuki.org>
2022-01-11 20:14:21 +0000

    Mumuki Export on 2022-01-11 20:14:21 +0000

--- 00004_Lectocomprensión/test.py
+++ 00004_Lectocomprensión/test.py
@@ -1,12 +1,11 @@
 
-  def setUp(self):
-    un_libro_corto = Libro("Socorro", 299, "Terror")
-    segundo_libro_corto = Libro("Fundación", 300, "Ciencia ficción")
-    tercer_libro_corto = Libro("Historias de cronopios y de famas", 153, "Cuento y novela")
-    un_libro_largo = Libro("Curtis Biología", 1164, "Ciencia")
-    segundo_libro_largo = Libro("El nudo de la conciencia", 464, "Divulgación")
-    tercer_libro_largo = Libro("La insoportable levedad del ser", 336, "Novela")
-    cuarto_libro_largo = Libro("El hobbit", 310, "Novela")
+  un_libro_corto = Libro("Socorro", 299, "Terror")
+  segundo_libro_corto = Libro("Fundación", 300, "Ciencia ficción")
+  tercer_libro_corto = Libro("Historias de cronopios y de famas", 153, "Cuento y novela")
+  un_libro_largo = Libro("Curtis Biología", 1164, "Ciencia")
+  segundo_libro_largo = Libro("El nudo de la conciencia", 464, "Divulgación")
+  tercer_libro_largo = Libro("La insoportable levedad del ser", 336, "Novela")
+  cuarto_libro_largo = Libro("El hobbit", 310, "Novela")
     
   def test_Si_a_una_biblioteca_con_tres_libros_largos_le_enviamos_el_mensaje_libros_largos_nos_retorna_esos_tres_libros(self):
     self.setUp()
