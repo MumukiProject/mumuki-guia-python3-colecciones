4ead3a62b1ece2b132fc10faf6a94af3b5dcd725
mayra@mumuki.org <mayra@mumuki.org>
2022-01-11 19:32:57 +0000

    Mumuki Export on 2022-01-11 19:32:57 +0000

--- 00004_Lectocomprensión/extra.py
+++ 00004_Lectocomprensión/extra.py
@@ -1,11 +1,4 @@
-class Libro:
-  def __init__(self, un_titulo, cant_paginas, un_genero):
-    self.titulo = un_titulo
-    self.paginas = cant_paginas
-    self.genero = un_genero
-    
-  def es_largo(self):
-    return self.paginas > 300
+
 
 un_libro_corto = Libro("Socorro", 299, "Terror")
 segundo_libro_corto = Libro("Fundación", 300, "Ciencia ficción")
