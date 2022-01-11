81015bdb022e760259d02e5d28d7042186c31580
mayra@mumuki.org <mayra@mumuki.org>
2022-01-11 19:10:08 +0000

    Mumuki Export on 2022-01-11 19:10:08 +0000

--- 00004_Lectocomprensión/test.py
+++ 00004_Lectocomprensión/test.py
@@ -1,4 +1,6 @@
-class Test(unittest.TestCase):
-
-  def test_description_example(self):
-    self.assertTrue(True)
+  
+  def test_Si_a_una_biblioteca_con_tres_libros_largos_le_enviamos_el_mensaje_libros_largos_nos_retorna_esos_tres_libros(self):
+    biblioteca = Biblioteca()
+    biblioteca.agregar_libro("Socorro")
+    biblioteca.agregar_libro("El lobo estepario")
+    self.assertEqual(biblioteca.libros, ["Socorro", "El lobo estepario"])
