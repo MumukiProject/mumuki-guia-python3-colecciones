c0826b84835d3264357407646e6be132c6b30e9f
gustavo@mumuki.org <gustavo@mumuki.org>
2022-01-12 16:05:26 +0000

    Mumuki Export on 2022-01-12 16:05:26 +0000

--- 00004_Lectocomprensión/test.py
+++ 00004_Lectocomprensión/test.py
@@ -19,9 +19,6 @@
     self.assertEqual(biblioteca.libros_largos(), [self.un_libro_largo, self.segundo_libro_largo, self.tercer_libro_largo, self.cuarto_libro_largo])
     
   def test_Si_a_una_biblioteca_con_ningún_libro_largos_le_enviamos_el_mensaje_libros_largos_nos_retorna_una_lista_vacía(self):
-    un_libro_corto = Libro("Socorro", 299, "Terror")
-    segundo_libro_corto = Libro("Fundación", 300, "Ciencia ficción")
-    tercer_libro_corto = Libro("Historias de cronopios y de famas", 153, "Cuento y novela")
     biblioteca = Biblioteca()
     biblioteca.libros = [self.un_libro_corto, self.segundo_libro_corto, self.tercer_libro_corto]
     self.assertEqual(biblioteca.libros_largos(), [])
