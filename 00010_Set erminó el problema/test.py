4f75fe1686c874f0606630b523f890821ecb863b
gustavo@mumuki.org <gustavo@mumuki.org>
2022-01-13 13:49:19 +0000

    Mumuki Export on 2022-01-13 13:49:19 +0000

--- 00010_Set erminó el problema/test.py
+++ 00010_Set erminó el problema/test.py
@@ -15,18 +15,18 @@
   def test_si_una_biblioteca_recibe_el_mensaje_agregar_libro_se_agrega_un_libro_a_sus_libros_si_no_lo_tiene(self):
     biblioteca = Biblioteca()
     biblioteca.agregar_libro(self.levedad)
-    self.assertEqual(biblioteca.libros, [self.levedad])
+    self.assertEqual(biblioteca.libros, {self.levedad})
 
   def test_si_una_biblioteca_recibe_el_mensaje_agregar_libro_dos_veces_se_agregan_dos_libros_a_sus_libros_si_no_los_tiene(self):
     biblioteca = Biblioteca()
     biblioteca.agregar_libro(self.socorro)
     biblioteca.agregar_libro(self.como_agua)
-    self.assertEqual(biblioteca.libros, [self.socorro, self.como_agua])
+    self.assertEqual(biblioteca.libros, {self.socorro, self.como_agua})
 
   def test_si_una_biblioteca_recibe_el_mensaje_agregar_libro_no_hace_nada_si_ya_tiene_el_libro(self):
     biblioteca = Biblioteca()
     biblioteca.agregar_libro(self.cronopios)
     biblioteca.agregar_libro(self.como_agua)
-    self.assertEqual(biblioteca.libros, [self.cronopios, self.como_agua])
+    self.assertEqual(biblioteca.libros, {self.cronopios, self.como_agua})
     biblioteca.agregar_libro(self.cronopios)
-    self.assertEqual(biblioteca.libros, [self.cronopios, self.como_agua])
+    self.assertEqual(biblioteca.libros, {self.cronopios, self.como_agua})
