fa112bdb5dc374142684dbf68b37ba825a632adc
gustavo@mumuki.org <gustavo@mumuki.org>
2022-01-13 19:11:35 +0000

    Mumuki Export on 2022-01-13 19:11:35 +0000

--- 00012_Se viene el tupla tupla/test.py
+++ 00012_Se viene el tupla tupla/test.py
@@ -1,12 +1,12 @@
 
-  def test_anterior_y_siguiente_de_10_son_9_y_11(self):
-    self.assertEqual(anterior_y_siguiente(10), (9, 11))
+  def test_las_iniciales_de_Jorge_Luis_Borges_son_J_L_y_B(self):
+    borges = Persona("Jorge", "Luis", "Borges")
+    self.assertEqual(borges.iniciales(), ("J", "L", "B"))
     
-  def test_anterior_y_siguiente_de_5_son_4_y_6(self):
-    self.assertEqual(anterior_y_siguiente(5), (4, 6))
+  def test_las_iniciales_de_Haydeé_Mercedes_Sosa_son_H_M_y_S(self):
+    borges = Persona("Haydée", "Mercedes", "Sosa")
+    self.assertEqual(borges.iniciales(), ("H", "M", "S"))
     
-  def test_anterior_y_siguiente_de_2_son_1_y_3(self):
-    self.assertEqual(anterior_y_siguiente(2), (1, 3))
-  
-  def test_anterior_y_siguiente_de_7_son_6_y_8(self):
-    self.assertEqual(anterior_y_siguiente(7), (6, 8))
+  def test_las_iniciales_de_Luis_Alberto_Spinetta_son_L_A_y_S(self):
+    borges = Persona("Luis", "Alberto", "Spinetta")
+    self.assertEqual(borges.iniciales(), ("L", "A", "S"))
