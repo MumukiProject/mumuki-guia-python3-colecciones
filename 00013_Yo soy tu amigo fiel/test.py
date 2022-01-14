3e431d1cbde8d1f2f7ab04cdff5fc8c66b4db7f3
mayra@mumuki.org <mayra@mumuki.org>
2022-01-14 13:19:12 +0000

    Mumuki Export on 2022-01-14 13:19:12 +0000

--- 00013_Yo soy tu amigo fiel/test.py
+++ 00013_Yo soy tu amigo fiel/test.py
@@ -2,3 +2,18 @@
   def test_un_juguete_con_precio_minorista_mayor_a_1000_y_precio_mayorista_menor_a_600_no_es_barato(self):
     pizarra_magica = Juguete("Pizarra mágica", 1001, 599, set([{"pieza": "pizarra", "tamaño": "grande"}]))
     self.assertFalse(pizarra_magica.es_barato())
+    
+
+  def test_Un_juguete_con_precio_minorista_menor_a_1000_y_precio_mayorista_menor_a_600_es_barato(self):
+    buzz = Juguete("Buzz Lightyear", 800, 500, set([{"pieza": "cabeza", "tamaño": "grande"}, {"pieza": "cuerpo", "tamaño": "grande"}])
+    self.assertTrue(buzz.es_barato())
+
+
+#  Un juguete con precio minorista menor a 1000 y precio mayorista menor a 600 es barato.
+#Un juguete con precio minorista mayor a 1000 y precio mayorista menor a 600 no es barato.
+#Un juguete con precio minorista menor a 1000 y precio mayorista mayor a 600 no es barato.
+#precios_con_iva de un juguete con precio minorista XX y mayorista YY retorna la tupla (ZZ, TT).
+#precios_con_iva de un juguete con precio minorista XX y mayorista YY retorna la tupla (ZZ, TT).
+#Un juguete que tiene todas partes grandes es apto para todo público.
+#Un juguete que tiene alguna parte que no es grande no es apto para todo público.
+
